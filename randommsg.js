2a1c0f8a01bc93dcfb3095bf51441747a19d0d78
Piotr Wrobel <piotr@retroroad.co.uk>
2024-06-03 08:09:17 +0100

    added time

--- randommsg.js
+++ randommsg.js
@@ -1,5 +1,10 @@
 //random_msg
 console.log('hi');
+
+//getting current time (hours)
+let date = new Date();
+let time = date.getHours();
+
 let msg = ['test','test'];
 let msg2 = [];
 
@@ -8,5 +13,5 @@ function random(arr){
     console.log(msg[(Math.floor(Math.random() * arr.length))]);
 }
 
-
+console.log(time);
 random(msg);
