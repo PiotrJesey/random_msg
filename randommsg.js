a4b7e77dbe617e6e4f7d004b04b84eafd59a20f6
Piotr Wrobel <piotr@retroroad.co.uk>
2024-06-03 07:15:35 +0100

    random function and 2 arrays

--- randommsg.js
+++ randommsg.js
@@ -1,2 +1,12 @@
 //random_msg
 console.log('hi');
+let msg = ['test','test'];
+let msg2 = [];
+
+function random(arr){
+
+    console.log(msg[(Math.floor(Math.random() * arr.length))]);
+}
+
+
+random(msg);
