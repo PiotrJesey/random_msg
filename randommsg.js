03457db973e68a7707e997d47c51af8726b78bd1
Piotr Wrobel <piotr@retroroad.co.uk>
2024-06-04 07:32:24 +0100

    new functions

--- randommsg.js
+++ randommsg.js
@@ -2,16 +2,30 @@
 console.log('hi');
 
 //getting current time (hours)
+function Greetings(){
 let date = new Date();
 let time = date.getHours();
+if (time < 12){
+    return 'Good morning!'
+} else{
+    return 'Good afternoon!'
+}
+}
 
-let msg = ['test','test'];
+let msg = ['We cannot solve problems with the kind of thinking we employed when we came up with them. —Albert Einsteinest','Learn as if you will live forever, live like you will die tomorrow. —Mahatma Gandhi', 'Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too. —Mark Twain'];
 let msg2 = [];
 
 function random(arr){
 
-    console.log(msg[(Math.floor(Math.random() * arr.length))]);
+    return msg[(Math.floor(Math.random() * arr.length))];
 }
 
-console.log(time);
-random(msg);
+let RandomMessage = (name) => {
+    
+    console.log(  `${Greetings()}, ${name}`);
+    console.log('this is message to you!'.toUpperCase());
+    console.log(random(msg)+'\n'+'sd')
+}
+
+
+RandomMessage('Piotr')
